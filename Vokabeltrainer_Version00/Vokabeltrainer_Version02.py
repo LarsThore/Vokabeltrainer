9567252d027d9bc68470038fe4b4fa30fe0bb63c
LarsThore <thore.bergmann@fau.de>
2017-03-19 15:15:46 +0100

    Trotzdem-Button eingefügt und Level werden beim Antworten aktualisiert.

--- Vokabeltrainer_Version00/Vokabeltrainer_Version02.py
+++ Vokabeltrainer_Version00/Vokabeltrainer_Version02.py
@@ -2,8 +2,8 @@
 
 from PyQt5.QtCore import Qt, QRectF, QEvent
 from PyQt5.QtGui import (QColor, QPen, QBrush, QCursor, QImage,
-    QPainter, QTextCursor)
-from PyQt5.QtWidgets import QWidget, QApplication
+    QPainter, QTextCursor, QKeySequence)
+from PyQt5.QtWidgets import QWidget, QApplication, QShortcut
 from PyQt5.uic import loadUi
 
 from types import MethodType
@@ -13,8 +13,12 @@ import numpy
 # TODO: "Trotzdem Richtig" - Button einführen
 # TODO: Vokabelliste in zweiten Tab einfügen
 # TODO: Level richtig aktualisieren
+#           - bei richtigen Antworten    -----   DONE
+#           - bei falschen Antworten    ----- Do Nothing
 # TODO: Level richtig speichern
-# TODO: Funktion/Programm zum Importieren von Vokabellisten einfügen 
+# TODO: Tooltipps einfügen
+# TODO: mehr Wörter der unteren Level auswählen
+# TODO: Funktion/Programm zum Importieren von Vokabellisten einfügen
 
 ############################### load dictionary ###############################
 
@@ -68,19 +72,28 @@ def get_next_word():
     # fill the first text-edit-box with a word from the dictionary
     widget.lineEdit_1.setText(french[e])
 
+    # show the level value
+    values = french_german[french[e]]
+    level_text = str(values[1])
+    widget.label_level.setText(level_text)
+
     # set focus on second line edit
     widget.lineEdit_2.setFocus()
 
     return french[e]
 
 
-def check_entry(word1, word2):
+def check_entry(word1, word2, word3):
     '''
     Returns True if the word1 and word2 are a key-value-pair and False if not.
     '''
-
+    # extract correct word from the list in dictionary according to
+    # first line edit -- second language -- correct word
     word1 = str(word1[0])
+    # the word in second line edit -- second language -- tested word
     word2 = str(word2)
+    # the respective word in - first language
+    word3 = str(word3)
 
     print(word1, word2)
 
@@ -88,11 +101,22 @@ def check_entry(word1, word2):
         print (True)
         widget.label.setText("Richtig!")
         get_next_word()
+
+        # Textfelder neu einrichten
         widget.lineEdit_2.clear()
         widget.lineEdit_2.setFocus()
+
+        # Level aktualisieren
+        values = french_german[word3]
+        level = int(values[1])
+        french_german[word3] = [word2, level + 1]
+        return True
     else:
         print (False)
         widget.label.setText("Leider Falsch!")
+        widget.lineEdit_2.setFocus()
+        widget.lineEdit_2.selectAll()
+        return False
 
 
 # set text of text labels for language 1 and language 2
@@ -103,11 +127,20 @@ widget.label_2.setText("   Deutsch")
 word = get_next_word()
 
 # after entering the word check (with function) if entry was correct
-widget.lineEdit_2.returnPressed.connect(lambda:
+answer = widget.lineEdit_2.returnPressed.connect(lambda:
     check_entry(french_german[widget.lineEdit_1.text()],
-                widget.lineEdit_2.text()))
+                widget.lineEdit_2.text(), widget.lineEdit_1.text()))
+
+if answer == True:
+    pass
+else:
+    if widget.pushButton.clicked or widget.pushButton.returnPressed:
+        widget.pushButton.clicked.connect(get_next_word)
 
 
+# close the widget with shortcut (ESCAPE KEY)
+shortcut = QShortcut(QKeySequence(Qt.Key_Escape), widget)
+shortcut.activated.connect(widget.close)
 
 # start widget
 widget.show()
